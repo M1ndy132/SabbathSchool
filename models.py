d1ef75ed1e57899ac95f532837a4488940b3f71e
M1ndy <gdm1ndy234@gmail.com>
2026-01-06 18:18:46 -0500

    Refactor class structure: replace WeeklyTally with Class_Entry and update SabbathSchoolClass and Division initializers

--- models.py
+++ models.py
@@ -1,20 +1,21 @@
-class WeeklyTally:
-    def __init__(self) -> None:
-        self.week_number = 0
-        self.attendance = 0
-        self.study = 0
-        self.vistors = 0
-        self.offering = 0.0
+class Class_Entry:
+    def __init__(self, date, class_name, attendance = 0, study = 0, vistors = 0, offering = 0.0) -> None:
+        self.date = date
+        self.class_name = class_name
+        self.attendance = attendance
+        self.study = study
+        self.vistors = vistors
+        self.offering = offering
 
 
 class SabbathSchoolClass:
-    def __init__(self) -> None:
-        self.class_name = ""
-        self.weekly_tallys = []
+    def __init__(self, class_name) -> None:
+        self.class_name = class_name
+        self.entries = []
 
 
 class Division:
-    def __init__(self) -> None:
-        self.division_name = ""
+    def __init__(self, division_name) -> None:
+        self.division_name = division_name
         self.classes = []
 
