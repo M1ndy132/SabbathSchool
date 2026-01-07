a96fa1caad9674e4c36d1a88fd92ed69deeb68ad
M1ndy <gdm1ndy234@gmail.com>
2026-01-06 19:09:07 -0500

    Add sample Sabbath date entries for Upper and Lower Division classes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from models import Class_Entry, Division, SabbathSchoolClass
+from datetime import date
 
 UpperDivision = Division("Upper Division")
 LowerDivision = Division("Lower Division")
@@ -10,3 +11,28 @@ for name in unit_names:
 class_names = ["Kindergarten", "Primary", "Junior", "Earliteen", "Cornerstone"]
 for name in class_names:
     LowerDivision.classes.append(SabbathSchoolClass(name))
+
+Sabbath_Date = date(2026, 1, 10)
+
+for cls in UpperDivision.classes:
+    entry = Class_Entry(
+        date=Sabbath_Date,
+        class_name =cls.class_name,
+        attendance = 8,
+        study=5,
+        vistors=1,
+        offering=1000.0
+    )
+    cls.entries.append(entry)
+
+
+for cls in LowerDivision.classes:
+    entry = Class_Entry(
+        date=Sabbath_Date,
+        class_name=cls.class_name,
+        attendance=14,
+        study=10,
+        vistors=5,
+        offering=500.0
+    )
+    cls.entries.append(entry)
